62d9dac0c9b491aee9a91d35d9f3ff84ab192ec2
Josh <zylo.codes@gmail.com>
2022-07-12 19:44:26 -0500

    Configures ApolloServer

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express')
+const connection = require('./config/connection')
+const { typeDefs, resolvers } = require('./schemas')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,6 +18,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Express server running on port ${PORT}!`);
-});
+connection.once('open', async () => {
+  const server = new ApolloServer({ typeDefs, resolvers })
+  await server.start()
+  server.applyMiddleware({ app })
+  app.listen(PORT, () => {
+    console.log(`Express server running on port ${PORT}!`);
+    console.log(`Visit graphql playground at http://localhost:${PORT}${server.graphqlPath}`)
+  });
+})
