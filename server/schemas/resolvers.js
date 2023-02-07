92fdc2605e19bef1401a9db447206c38e269ee19
Josh <zylo.codes@gmail.com>
2023-02-06 21:24:43 -0600

    Query resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,18 @@
+const { Pokemon, Trainer } = require('../models')
+
 const resolvers = {
   Query: {
     pokemons: async (parent, args, context, info) => {
-
+      return await Pokemon.find()
     },
     pokemon: async (parent, args, context, info) => {
-
+      return await Pokemon.findOne({ pokemonId: args.pokemonId })
     },
     trainers: async (parent, args, context, info) => {
-
+      return await Trainer.find()
     },
     trainer: async (parent, args, context, info) => {
-
+      return await Trainer.findById(args._id)
     },
   },
 }
