15b0bb7f279cc3a3b7db1dbe3458f1e4f3fa8326
Jesus M <marquez.jay444@gmail.com>
2023-02-08 18:40:46 -0600

    update

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const typeDefs = `
+  type Pokemon {
+    _id: ID 
+    pokemonId: Int
+    name: String
+    height: Int
+    weight: Int
+    base_experience: Int
+    image: String
+    moves: [String]
+  }
+
+  type Trainer {
+    _id: ID
+    username: String
+    email: String
+    pokemon: [Pokemon]
+  }
+
+  type Query {
+    pokemons: [Pokemon]
+    pokemon(pokemonId: Int!): Pokemon
+    trainers: [Trainer]
+    trainer(_id: ID!): Trainer
+  }
+
+  type Mutation {
+    addPokemon(
+      trainerId: ID!
+      pokemonId: Int!
+      name: String!
+      height: Int
+      weight: Int
+      base_experience: Int
+      image: String
+      moves: [String]
+    ): Pokemon
+
+    addTrainer(
+      username: String!
+      email: String!
+      password: String!
+    ): Trainer
+  }
+`
+
+module.exports = typeDefs
